758fe9e027e499eb43d16ade6d3b1b3362eb9986
Adam Harmansky <adam.harmansky@gmail.com>
2023-04-01 13:10:06 +0200

    sex penis

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -8,7 +8,7 @@
     let map;
     let placeData = null;
 
-    export let id;
+    export let selected_id;
 
     onMount(async () => {
         if (!browser) return;
@@ -16,57 +16,21 @@
         let center = [48.72, 21.26];
         let map = create_map(L, mapElement, center);
 
-        var bussin = L.icon({
-            iconUrl: '/bussin.svg',
-            iconSize: [40, 40],
-            iconAnchor: [20, 20],
-        });
-
-        var skola = L.icon({
-            iconUrl: '/skola.svg',
-            iconSize: [40, 40],
-            iconAnchor: [20, 20],
-        });
-
-        var skolka = L.icon({
-            iconUrl: '/skolka.svg',
-            iconSize: [40, 40],
-            iconAnchor: [20, 20],
-        });
-
-        var spital = L.icon({
-            iconUrl: '/spital.svg',
-            iconSize: [40, 40],
-            iconAnchor: [20, 20],
-        });
-
-        var dom = L.icon({
-            iconUrl: '/dom.svg',
-            iconSize: [52, 52],
-            iconAnchor: [26, 26],
-            className: 'dom',
-        });
-
-        var pes = L.icon({
-            iconUrl: '/pes.svg',
-            iconSize: [40, 40],
-            iconAnchor: [20, 20],
-        });
-
-        var velky_dom = L.icon({
-            iconUrl: '/dom.svg',
-            iconSize: [52, 52],
-            iconAnchor: [26, 26],
-        });
-        var apatieka = L.icon({
-            iconUrl: '/apatieka.svg',
-
-            iconSize: [40, 40],
-            iconAnchor: [20, 20],
-        });
+        let icons = {
+            bussin: '/bussin.svg',
+            skola: '/skola.svg',
+            skolka: '/skolka.svg',
+            spital: '/spital.svg',
+            dom: '/dom.svg'
+            pes: '/pes.svg',
+            velky_dom: '/dom.svg',
+            apatieka: '/apatieka.svg'
+        };
+        for (let i in icons) {
+            icons[i] = {iconUrl: i, iconSize: [40, 40], iconAnchor: [20, 20]};
+        }
 
         let markers = [];
-
         let isoch;
 
         async function update_isoch(id) {
@@ -83,30 +47,6 @@
             console.log(data);
             isoch = L.polygon(data.isochrone).addTo(map);
         }
-
-        /*
-        async function update_isoch(lat, lon) {
-            const response = await fetch('/mapa/isochrone', {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    lat: lat,
-                    lon: lon,
-                    time: 600,
-                    transportation: 'driving',
-                }),
-            });
-            const data = await response.json();
-            if (izoch) {
-                izoch.remove();
-            }
-            console.log(data);
-            izoch = L.polygon(data.outline).addTo(map);
-        }
-        */
 
         var point;
 
@@ -134,207 +74,6 @@
             for (let i in markers) {
                 markers[i].remove();
             }
-
-            veci.zakladne_skoly.forEach(i => {
-                markers.push(
-                    L.marker(
-                        [i.geometry.coordinates[1], i.geometry.coordinates[0]],
-                        { icon: skola }
-                    )
-                        .addTo(map)
-                        .bindPopup(`<h3>${i.properties.organizacia_nazov}</h3>`)
-                );
-            });
-            veci.stredne_skoly.forEach(i => {
-                markers.push(
-                    L.marker(
-                        [i.geometry.coordinates[1], i.geometry.coordinates[0]],
-                        { icon: skola }
-                    )
-                        .addTo(map)
-                        .bindPopup(`<h3>${i.properties.organizacia_nazov}</h3>`)
-                );
-            });
-            veci.materske_skoly.forEach(i => {
-                markers.push(
-                    L.marker(
-                        [i.geometry.coordinates[1], i.geometry.coordinates[0]],
-                        { icon: skolka }
-                    )
-                        .addTo(map)
-                        .bindPopup(`<h3>${i.properties.organizacia_nazov}</h3>`)
-                );
-            });
-            veci.zastavky.forEach(i => {
-                markers.push(
-                    L.marker(
-                        [i.geometry.coordinates[1], i.geometry.coordinates[0]],
-                        { icon: bussin }
-                    )
-                        .addTo(map)
-                        .bindPopup(
-                            `<h3> ${i.properties.zastavka_nazov}</h3> Zastávka`
-                        )
-                );
-            });
-            veci.psy.forEach(i => {
-                markers.push(
-                    L.marker(
-                        [i.geometry.coordinates[1], i.geometry.coordinates[0]],
-                        { icon: pes }
-                    )
-                        .addTo(map)
-                        .bindPopup(`<h3>psíčkarska zóna</h3>`)
-                );
-            });
-            veci.lekarne.forEach(i => {
-                markers.push(
-                    L.marker(
-                        [i.geometry.coordinates[1], i.geometry.coordinates[0]],
-                        { icon: apatieka }
-                    )
-                        .addTo(map)
-                        .bindPopup(`<h3>${i.properties.nazov}</h3>`)
-                );
-            });
-            veci.ambulancie.forEach(i => {
-                markers.push(
-                    L.marker(
-                        [i.geometry.coordinates[1], i.geometry.coordinates[0]],
-                        { icon: spital }
-                    )
-                        .addTo(map)
-                        .bindPopup(`<h3>${i.properties.nazov_zariadenia}</h3>`)
-                );
-            });
-
-            let d_zs = node_distance(lat, lon, veci.zakladne_skoly[0]);
-            let d_ss = node_distance(lat, lon, veci.stredne_skoly[0]);
-            let d_ms = node_distance(lat, lon, veci.materske_skoly[0]);
-            let d_z = node_distance(lat, lon, veci.zastavky[0]);
-            let d_p = node_distance(lat, lon, veci.psy[0]);
-            let d_l = node_distance(lat, lon, veci.lekarne[0]);
-            let d_a = node_distance(lat, lon, veci.ambulancie[0]);
-
-            console.log(extra_data);
-
-            placeData = {
-                name1: `${extra_data.druh}, ${extra_data.ulica}`,
-                name2: `${extra_data.cena}€ - ${extra_data.plocha}m²`,
-                name3: second,
-                properties: [
-                    (async () => {
-                        return {
-                            key: 'Najbližšia základná škola',
-                            value: `${d_zs}m - ${veci.zakladne_skoly[0].properties.organizacia_nazov}`,
-                            color:
-                                d_zs < 300
-                                    ? 'green'
-                                    : d_zs < 700
-                                    ? 'yellow'
-                                    : 'red',
-                        };
-                    })(),
-                    (async () => {
-                        return {
-                            key: 'Najbližšia stredná škola',
-                            value: `${d_ss}m - ${veci.stredne_skoly[0].properties.organizacia_nazov}`,
-                            color:
-                                d_ss < 700
-                                    ? 'green'
-                                    : d_ss < 1000
-                                    ? 'yellow'
-                                    : 'red',
-                        };
-                    })(),
-                    (async () => {
-                        return {
-                            key: 'Najbližšia materkská škola',
-                            value: `${d_ms}m - ${veci.materske_skoly[0].properties.organizacia_nazov}`,
-                            color:
-                                d_ms < 250
-                                    ? 'green'
-                                    : d_ms < 500
-                                    ? 'yellow'
-                                    : 'red',
-                        };
-                    })(),
-                    (async () => {
-                        return {
-                            key: 'Najbližšia zastávka MHD',
-                            value: `${d_z}m - ${veci.zastavky[0].properties.zastavka_nazov}`,
-                            color:
-                                d_z < 200
-                                    ? 'green'
-                                    : d_z < 500
-                                    ? 'yellow'
-                                    : 'red',
-                        };
-                    })(),
-                    (async () => {
-                        return {
-                            key: 'Najbližšie zariadenie pre psíčkarov',
-                            value: `${d_p}m`,
-                            color:
-                                d_p < 200
-                                    ? 'green'
-                                    : d_p < 500
-                                    ? 'yellow'
-                                    : 'red',
-                        };
-                    })(),
-                    (async () => {
-                        return {
-                            key: 'Najbližšia lekáreň',
-                            value: `${d_l}m - ${veci.lekarne[0].properties.nazov}`,
-                            color:
-                                d_l < 400
-                                    ? 'green'
-                                    : d_l < 800
-                                    ? 'yellow'
-                                    : 'red',
-                        };
-                    })(),
-                    (async () => {
-                        return {
-                            key: 'Najbližšia ambulancia',
-                            value: `${d_a}m - ${veci.ambulancie[0].properties.nazov_zariadenia}`,
-                            color:
-                                d_a < 300
-                                    ? 'green'
-                                    : d_a < 600
-                                    ? 'yellow'
-                                    : 'red',
-                        };
-                    })(),
-                ],
-            };
-
-            update_isoch(extra_data.id);
-        }
-
-        async function get_places() {
-            let request = await fetch('/mapa/byty');
-            let places = await request.json();
-            let byty = places.byty;
-            byty.forEach(i => {
-                let marker = L.marker([i.lat, i.lng], { icon: dom }).addTo(map);
-                marker.on('click', async e => {
-                    await update_data(e.latlng.lat, e.latlng.lng, i);
-                });
-            });
-
-            if (id != '') {
-                for (let i = 0; i < byty.length; i++) {
-                    if (byty[i].id == id) {
-                        await update_data(byty[i].lat, byty[i].lng, byty[i]);
-                        break;
-                    }
-                }
-            }
-        }
-
-        get_places();
     });
 
     onDestroy(async () => {
