a890913d33409d999cbd096f77e295f76fa9c26e
Plundration <matouinhell@gmail.com>
2023-04-01 13:12:58 +0200

    jkdfsla

--- src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import type { ServerLoad } from '@sveltejs/kit';
-
-// export const load: ServerLoad = ({ locals }) => {
-// 	if (locals.user && locals.user.profile) {
-// 		return {
-// 			profile: JSON.parse(JSON.stringify(locals.user.profile))
-// 		};
-// 	}
-// };
